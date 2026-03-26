87538935951e57dca6629069c083e8dc142e2bc8
lethanhhoa <lethanhhoa.cntt.anm@gmail.com>
2026-03-26 14:22:58 +0700

    feat: merge admin source code and setup routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,33 @@
-import { BrowserRouter as Router } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import AdminLogin from '../components/AdminLogin' 
+import HomePage from '../components/HomePage'
+import Dashboard from '../components/Dashboard'
+import Orders from '../components/OrderManagement'
+import Inventory from '../components/Inventory'
+import Customers from '../components/CustomerDirectory'
+import './App.css'
 
 function App() {
   return (
-    <Router>
-      <div className="min-h-screen bg-blue-100">
-        <h1 className="text-center text-4xl font-bold text-green-800 py-20">
-          Project E-commerce ReactJS - Đang xây dựng...
-        </h1>
-        <p className="text-center text-yellow-600">
-          Chúng ta sẽ bắt đầu từ Authentication → Router → Context
-        </p>
-      </div>
-    </Router>
+    <Routes>
+      <Route path="/" element={
+        <div className="min-h-screen bg-blue-100">
+          <h1 className="text-center text-4xl font-bold text-green-800 py-20">
+            Project E-commerce ReactJS - Đang xây dựng...
+          </h1>
+          <p className="text-center text-yellow-600">
+            Chúng ta sẽ bắt đầu từ Authentication → Router → Context
+          </p>
+        </div>
+      } />
+
+      <Route path="/admin" element={<AdminLogin />} />
+      <Route path="/homepage" element={<HomePage />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/order" element={<Orders />} />
+      <Route path="/inventory" element={<Inventory />} />
+      <Route path='/customer' element={<Customers />} />
+    </Routes>
   );
 }
 
