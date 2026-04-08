3c5f9057fc9dc28e31302c68a573674a5e80e153
lethanhhoa <lethanhhoa.cntt.anm@gmail.com>
2026-04-08 15:05:55 +0700

    add route protected for admin and all

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -4,27 +4,42 @@ import { createContext, useState, useContext } from "react";
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState(() => {
+    const saved = localStorage.getItem("user");
+    return saved ? JSON.parse(saved) : null;
+  });
   const [loading, setLoading] = useState(false);
 
   const login = async (username, password) => {
     setLoading(true);
     try {
-      const res = await fetch("https://69cfba0fa4647a9fc675e215.mockapi.io/users");
-      const users = await res.json();
+      const [resCustomer, resAdmin] = await Promise.all([
+        fetch("https://69cfba0fa4647a9fc675e215.mockapi.io/users"),
+        fetch("https://mindx-mockup-server.vercel.app/api/resources/APT-Project-Ecomerce?apiKey=69bc8f883d77cdfa59f97d31")
+      ]);
+      
+      const users = await resCustomer.json();
+      const admins = await resAdmin.json();
 
-      // Tìm user theo username và password_display
-      const foundUser = users.find(u =>
-        u.username === username && u.password_display === password
-      );
-
-      if (foundUser) {
-        setUser(foundUser);
-        localStorage.setItem("user", JSON.stringify(foundUser));
-        return { success: true, user: foundUser };
-      } else {
-        throw new Error("Tên đăng nhập hoặc mật khẩu không đúng");
+      // Kiểm tra Admin
+      const foundAdmin = admins.find(u => u.username === username && u.password_display === password);
+      if (foundAdmin) {
+        const data = { ...foundAdmin, role: 'admin' };
+        setUser(data);
+        localStorage.setItem("user", JSON.stringify(data));
+        return { success: true, role: 'admin' };
       }
+
+      // Kiểm tra User
+      const foundUser = users.find(u => u.username === username && u.password_display === password);
+      if (foundUser) {
+        const data = { ...foundUser, role: 'customer' };
+        setUser(data);
+        localStorage.setItem("user", JSON.stringify(data));
+        return { success: true, role: 'customer' };
+      }
+
+      throw new Error("Tên đăng nhập hoặc mật khẩu không đúng");
     } catch (error) {
       throw new Error(error.message || "Đăng nhập thất bại");
     } finally {
@@ -36,12 +51,6 @@ export const AuthProvider = ({ children }) => {
     setUser(null);
     localStorage.removeItem("user");
   };
-
-  // Tự động load user khi refresh trang
-  // useEffect(() => {
-  //   const savedUser = localStorage.getItem("user");
-  //   if (savedUser) setUser(JSON.parse(savedUser));
-  // }, []);
 
   return (
     <AuthContext.Provider value={{ user, login, logout, loading }}>
